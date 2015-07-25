d8fc305442762885f7c65da378c5e4a4ae1368ad
celanajaya <petermsteele@gmail.com>
2015-07-25 14:42:51 -0400

    changes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,11 +1,24 @@
 'use strict';
 var router = require('express').Router();
 module.exports = router;
+var roomsModel = require("../../db/models/rooms.js");
 
-router.use('/members', require('./members'));
 
-// Make sure this is after all of
-// the registered routes!
-router.use(function (req, res) {
-    res.status(404).end();
+
+router.get('/', function(req, res) {
+	res.send(roomsModel.rooms);
+});
+
+router.get('/:roomName', function(req, res) {
+	var sent = false;
+	for (var room = 0; room < roomsModel.rooms.length; room++) {
+		if (roomsModel.rooms[room].name === req.params.roomName) {
+			console.log(roomsModel.rooms[room]);
+			res.send(roomsModel.rooms[room]);
+			sent = true;
+		}
+	}
+	if (!sent) {
+		res.sendStatus(404);
+	}
 });
